77f0ca8e15476bdc5e1d401c4b6b872cbacb975a
asa05z2 <a23ninitkd@gmail.com>
2024-09-15 16:08:28 +0900

    display in a list-form and fix up the appearance

--- WalletDiet/ResultView.swift
+++ WalletDiet/ResultView.swift
@@ -12,36 +12,58 @@ struct ResultView: View {  // 結果画面の定義
     @State var patterns: [PaymentPattern] = []
     
     var body: some View {
-        VStack {
-            Text("結果画面")
-                .font(.largeTitle)
-                .padding()
-            
-            Text("支払い金額 \(paymentAmount)")
-            
-            ForEach(patterns) { pattern in
-                HStack {
-                    Text("\(pattern.change)")
-                    Text("\(pattern.payment)")
+        
+            VStack {
+                HStack{
+                    Text("支払い金額")
+                        .padding()
+                        .font(.largeTitle)
+                    
+                    Text("\(paymentAmount)")
+                        .font(.largeTitle)
+                        .foregroundColor(.red)
+                    
+                    Text("円")
+                        .padding()
+                        .font(.largeTitle)
+                }
+                
+                
+                Text("コインの枚数差が大きい順")
+                    .font(.title2)
+                    .padding()
+                
+                
+                
+                List{
+                    ForEach(patterns) { pattern in
+                        HStack {
+                            Text("\(pattern.change)円支払い")
+                                .font(.title3)
+                            
+                            Text("お釣り\(pattern.payment)円")
+                                .font(.title3)
+                        }//HStach
+                    }//ForEach
+                }//List
+                
+                
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()  // 画面を閉じるアクション
+                }) {
+                    Text("戻る")
+                        .font(.body)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
                 }
             }
-            
-            Button(action: {
-                self.presentationMode.wrappedValue.dismiss()  // 画面を閉じるアクション
-            }) {
-                Text("戻る")
-                    .font(.body)
-                    .padding()
-                    .background(Color.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+            //.navigationTitle("Result画面")  // 画面タイトル
+            .onAppear {
+                //paymentAmountを 関数に 渡して patterns に代入する。
+                patterns = calculator.calculateCharge(from: paymentAmount)
             }
-        }
-        //.navigationTitle("Result画面")  // 画面タイトル
-        .onAppear {
-            //paymentAmountを 関数に 渡して patterns に代入する。
-            patterns = calculator.calculateCharge(from: paymentAmount)
-        }
     }
 }
 
